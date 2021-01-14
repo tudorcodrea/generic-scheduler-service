1bed5fc517d684730439ca819d7553a6c6a60729
tudorcodrea <tudor.codrea@softvision.com>
2021-01-14 13:50:11 +0200

    Safeguarding to multiple Job impl with same simpleName

--- src/main/java/org/community/scheduler/util/JobRegistry.java
+++ src/main/java/org/community/scheduler/util/JobRegistry.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
+import org.community.scheduler.exception.GenericSchedulerException;
 import org.quartz.Job;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,9 +28,15 @@ public class JobRegistry {
 	private Map<String, Class<? extends Job>> registry;
 
 	@PostConstruct
-	public void initializeRegistry() {
-		registry = availableJobs.stream()
+	public void initializeRegistry() throws GenericSchedulerException {
+		
+		registry = availableJobs.stream().distinct()
 				.collect(Collectors.toMap(job -> job.getClass().getSimpleName(), job -> job.getClass()));
+		
+		if (registry.size() != availableJobs.size()) {
+			throw new GenericSchedulerException("Duplicate Quartz Job classes on the classpath detected !");
+		}
+		
 	}
 	
 	public Optional<Class<? extends Job>> findJobByName(String name){
